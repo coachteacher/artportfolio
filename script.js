4b68304ac38622b2b1cbeb6d1e7b015e851771d5
coachteacher <79242397+coachteacher@users.noreply.github.com>
2021-10-10 00:00:38 -0400

    upadated portfolio functions
    the portfolios open and close. Added info to CV and About sections. Updated all markup

--- script.js
+++ script.js
@@ -15,126 +15,101 @@ window.onload = function(){
     const currentDrawGallery = document.getElementById('currentDrawingLightBox');
     const olderWorkGallery = document.getElementById('olderWorkLightBox');
     const studiesGallery = document.getElementById('studiesLightBox');
-    
+    const currentPaintCurrentImage = document.getElementById('currentP-img');
+    const currentDrawCurrentImage = document.getElementById('currrentD-img');
+    const olderWorkCurrentImage = document.getElementById('currentO-img');
+    const studiesCurrentImage = document.getElementById('currentS-img');
+    const pImages = document.querySelectorAll('.p-images img');
+    const dImages = document.querySelectorAll('.d-images img');
+    const oImages = document.querySelectorAll('.o-images img');
+    const sImages = document.querySelectorAll('.s-images img');
+    const paintingInfo = document.getElementById('paintingImageInfo');
+
     //event listeners
     portfolioLink.addEventListener('click', () =>{
         portfolioSection.style.visibility = 'visible';
         aboutSection.style.visibility = 'hidden';
         cvSection.style.visibility = 'hidden';
         contactSection.style.visibility = 'hidden';
+        currentPaintGallery.style.visibility = 'hidden';
+        currentDrawGallery.style.visibility = 'hidden';
+        olderWorkGallery.style.visibility = 'hidden';
+        studiesGallery.style.visibility = 'hidden';
     })
     aboutLink.addEventListener('click', () =>{
         portfolioSection.style.visibility = 'hidden';
         aboutSection.style.visibility = 'visible';
         cvSection.style.visibility = 'hidden';
         contactSection.style.visibility = 'hidden';
+        currentPaintGallery.style.visibility = 'hidden';
+        currentDrawGallery.style.visibility = 'hidden';
+        olderWorkGallery.style.visibility = 'hidden';
+        studiesGallery.style.visibility = 'hidden';
     })
     cvLink.addEventListener('click', () =>{
         portfolioSection.style.visibility = 'hidden';
         aboutSection.style.visibility = 'hidden';
         cvSection.style.visibility = 'visible';
         contactSection.style.visibility = 'hidden';
+        currentPaintGallery.style.visibility = 'hidden';
+        currentDrawGallery.style.visibility = 'hidden';
+        olderWorkGallery.style.visibility = 'hidden';
+        studiesGallery.style.visibility = 'hidden';
     })
     contactLink.addEventListener('click', () =>{
         portfolioSection.style.visibility = 'hidden';
         aboutSection.style.visibility = 'hidden';
         cvSection.style.visibility = 'hidden';
         contactSection.style.visibility = 'visible';
+        currentPaintGallery.style.visibility = 'hidden';
+        currentDrawGallery.style.visibility = 'hidden';
+        olderWorkGallery.style.visibility = 'hidden';
+        studiesGallery.style.visibility = 'hidden';
     })
     currentPaintLink.addEventListener('click', () => {
         currentPaintGallery.style.visibility = 'visible';
-        portfolioSection.style.visibility = 'hidden';
     })
     currentDrawLink.addEventListener('click', () =>{
         currentDrawGallery.style.visibility = 'visible';
-        portfolioSection.style.visibility = 'hidden';
     })
     olderWorkLink.addEventListener('click', () => {
         olderWorkGallery.style.visibility = 'visible';
-        portfolioSection.style.visibility = 'hidden';
     })
     studiesLink.addEventListener('click', () =>{
         studiesGallery.style.visibility = 'visible';
-        portfolioSection.style.visibility = 'hidden';
     })
-
-
-
-
-
-
-    /*
-    //clicking to get the lightbox
-    const modal = document.getElementById('simpleModal');
-    const modalBtn = document.getElementById('modalBtn');
-    const closeBtn = document.querySelector('.closeBtn');
+    pImages.forEach(img => {
+        img.addEventListener('click', (e) => {
+            currentPaintCurrentImage.src = e.target.src;
+            pImages.forEach(img => (img.style.opacity = 1));
+            e.target.style.opacity = 0.6;
+        });
+    })
+    dImages.forEach(img => {
+        img.addEventListener('click', (e) => {
+            currentDrawCurrentImage.src = e.target.src;
+            dImages.forEach(img => (img.style.opacity = 1));
+            e.target.style.opacity = 0.6;
+        });
+    })
+    oImages.forEach(img => {
+        img.addEventListener('click', (e) => {
+            olderWorkCurrentImage.src = e.target.src;
+            oImages.forEach(img => (img.style.opacity = 1));
+            e.target.style.opacity = 0.6;
+        });
+    })
+    sImages.forEach(img => {
+        img.addEventListener('click', (e) => {
+            studiesCurrentImage.src = e.target.src;
+            sImages.forEach(img => (img.style.opacity = 1));
+            e.target.style.opacity = 0.6;
+        });
+    })
+    //functions
     
 
-    modalBtn.addEventListener('click', openModal);
-    closeBtn.addEventListener('click', closeModal);
-    window.addEventListener('click', clickOutside);
 
-        function openModal(){
-            modal.style.display = "block";
-        }
-        
-        function closeModal(){
-            modal.style.display = "none";
-        }
 
-        function clickOutside(e){
-            if(e.target == modal){
-            modal.style.display = "none";
-            }
-        }
-    
-    //image slider
-    let sliderImages = document.querySelectorAll('.slide');
-    let arrowRight = document.querySelector('.arrow-right');
-    let arrowLeft = document.querySelector('.arrow-left');
-    let current = 0;
 
-    function reset(){
-        for(i = 0; i < sliderImages.length; i++){
-            sliderImages[i].style.display = 'none';
-        }
-    };
-    
-    //initialize slider
-    function startSlide(){
-        reset();
-        sliderImages[0].style.display = 'block';
-    }
-
-    //show previous
-    function slideLeft(){
-        reset();
-        sliderImages[current - 1].style.display = 'block';
-        current--;
-    }
-
-    //left arrow click
-    arrowLeft.addEventListener('click', function(){
-        if(current === 0){
-            current = sliderImages.length;
-        }
-        slideLeft();
-    });
-
-    //show next
-    function slideRight(){
-        reset();
-        sliderImages[current + 1].style.display = 'block';
-        current++;
-    }
-
-    //right arrow click
-    arrowRight.addEventListener('click', function(){
-        if(current === sliderImages.length -1){
-            current = -1;
-        }
-        slideRight();
-    });
-    
-    startSlide();*/
 };
