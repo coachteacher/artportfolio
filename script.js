cf68fcc47841b991a9f20c11286fe6e21c4475b2
Benjamin Rogers <79242397+coachteacher@users.noreply.github.com>
2021-10-12 01:40:25 -0400

    enhanced functionality
    title change is now included, along with a streamlined script

--- script.js
+++ script.js
@@ -3,14 +3,6 @@ window.onload = function(){
     const aboutSection = document.getElementById('aboutSection');
     const cvSection = document.getElementById('cvSection');
     const contactSection = document.getElementById('contactSection');
-    const portfolioLink = document.getElementById('portfolioSectionLink');
-    const aboutLink = document.getElementById('aboutSectionLink');
-    const cvLink = document.getElementById('cvSectionLink');
-    const contactLink = document.getElementById('contactSectionLink');
-    const currentPaintLink = document.getElementById('paintingsLink');
-    const currentDrawLink = document.getElementById('drawingsLink');
-    const olderWorkLink = document.getElementById('oldWorkLink');
-    const studiesLink = document.getElementById('studiesLink');
     const currentPaintGallery = document.getElementById('currentPaintingLightBox');
     const currentDrawGallery = document.getElementById('currentDrawingLightBox');
     const olderWorkGallery = document.getElementById('olderWorkLightBox');
@@ -25,64 +17,98 @@ window.onload = function(){
     const sImages = document.querySelectorAll('.s-images img');
     const paintingInfo = document.getElementById('paintingImageInfo');
 
-    //event listeners
-    portfolioLink.addEventListener('click', () =>{
-        portfolioSection.style.visibility = 'visible';
-        aboutSection.style.visibility = 'hidden';
-        cvSection.style.visibility = 'hidden';
-        contactSection.style.visibility = 'hidden';
-        currentPaintGallery.style.visibility = 'hidden';
-        currentDrawGallery.style.visibility = 'hidden';
-        olderWorkGallery.style.visibility = 'hidden';
-        studiesGallery.style.visibility = 'hidden';
-    })
-    aboutLink.addEventListener('click', () =>{
-        portfolioSection.style.visibility = 'hidden';
-        aboutSection.style.visibility = 'visible';
-        cvSection.style.visibility = 'hidden';
-        contactSection.style.visibility = 'hidden';
-        currentPaintGallery.style.visibility = 'hidden';
-        currentDrawGallery.style.visibility = 'hidden';
-        olderWorkGallery.style.visibility = 'hidden';
-        studiesGallery.style.visibility = 'hidden';
-    })
-    cvLink.addEventListener('click', () =>{
-        portfolioSection.style.visibility = 'hidden';
-        aboutSection.style.visibility = 'hidden';
-        cvSection.style.visibility = 'visible';
-        contactSection.style.visibility = 'hidden';
-        currentPaintGallery.style.visibility = 'hidden';
-        currentDrawGallery.style.visibility = 'hidden';
-        olderWorkGallery.style.visibility = 'hidden';
-        studiesGallery.style.visibility = 'hidden';
-    })
-    contactLink.addEventListener('click', () =>{
-        portfolioSection.style.visibility = 'hidden';
-        aboutSection.style.visibility = 'hidden';
-        cvSection.style.visibility = 'hidden';
-        contactSection.style.visibility = 'visible';
-        currentPaintGallery.style.visibility = 'hidden';
-        currentDrawGallery.style.visibility = 'hidden';
-        olderWorkGallery.style.visibility = 'hidden';
-        studiesGallery.style.visibility = 'hidden';
-    })
-    currentPaintLink.addEventListener('click', () => {
-        currentPaintGallery.style.visibility = 'visible';
-    })
-    currentDrawLink.addEventListener('click', () =>{
-        currentDrawGallery.style.visibility = 'visible';
-    })
-    olderWorkLink.addEventListener('click', () => {
-        olderWorkGallery.style.visibility = 'visible';
-    })
-    studiesLink.addEventListener('click', () =>{
-        studiesGallery.style.visibility = 'visible';
-    })
+    //site navigation
+    const navLinks = document.querySelectorAll('.navLinks')
+    navLinks.forEach(link => {
+        link.addEventListener('click', (e) => {
+                const portfolioLink = document.getElementById('portfolioSectionLink');
+                const aboutLink = document.getElementById('aboutSectionLink');
+                const cvLink = document.getElementById('cvSectionLink');
+                const contactLink = document.getElementById('contactSectionLink');
+        
+                switch(e.target){
+                    case portfolioLink:
+                        portfolioSection.style.visibility = 'visible';
+                        aboutSection.style.visibility = 'hidden';
+                        cvSection.style.visibility = 'hidden';
+                        contactSection.style.visibility = 'hidden';
+                        currentPaintGallery.style.visibility = 'hidden';
+                        currentDrawGallery.style.visibility = 'hidden';
+                        olderWorkGallery.style.visibility = 'hidden';
+                        studiesGallery.style.visibility = 'hidden';
+                        break;
+                    case aboutLink:
+                        portfolioSection.style.visibility = 'hidden';
+                        aboutSection.style.visibility = 'visible';
+                        cvSection.style.visibility = 'hidden';
+                        contactSection.style.visibility = 'hidden';
+                        currentPaintGallery.style.visibility = 'hidden';
+                        currentDrawGallery.style.visibility = 'hidden';
+                        olderWorkGallery.style.visibility = 'hidden';
+                        studiesGallery.style.visibility = 'hidden';
+                        break;
+                    case cvLink:
+                        portfolioSection.style.visibility = 'hidden';
+                        aboutSection.style.visibility = 'hidden';
+                        cvSection.style.visibility = 'visible';
+                        contactSection.style.visibility = 'hidden';
+                        currentPaintGallery.style.visibility = 'hidden';
+                        currentDrawGallery.style.visibility = 'hidden';
+                        olderWorkGallery.style.visibility = 'hidden';
+                        studiesGallery.style.visibility = 'hidden';
+                        break;
+                    case contactLink:
+                        portfolioSection.style.visibility = 'hidden';
+                        aboutSection.style.visibility = 'hidden';
+                        cvSection.style.visibility = 'hidden';
+                        contactSection.style.visibility = 'visible';
+                        currentPaintGallery.style.visibility = 'hidden';
+                        currentDrawGallery.style.visibility = 'hidden';
+                        olderWorkGallery.style.visibility = 'hidden';
+                        studiesGallery.style.visibility = 'hidden';
+                    }
+                })
+            })
+
+    //portfolio navigation
+    const portfolioLinks = document.querySelectorAll('.portfolioLinks');
+    portfolioLinks.forEach(link => {
+        link.addEventListener('click', (e) => {           
+            const currentPaintLink = document.getElementById('recentPaintingsLink');
+            const currentDrawLink = document.getElementById('recentDrawingsLink');
+            const olderWorkLink = document.getElementById('olderWorkLink');
+            const studiesLink = document.getElementById('studiesWorkLink');
+            const paintingsSpan = document.getElementById('paintingsSpan');
+            const drawingsSpan = document.getElementById('drawingsSpan');
+            const olderSpan = document.getElementById('olderSpan');
+            const studiesSpan = document.getElementById('studiesSpan');
+
+            switch(e.target){
+                case currentPaintLink:
+                case paintingsSpan:
+                    currentPaintGallery.style.visibility = 'visible';
+                    break;
+                case currentDrawLink:
+                case drawingsSpan:
+                    currentDrawGallery.style.visibility = 'visible';
+                    break;
+                case olderWorkLink:
+                case olderSpan:
+                    olderWorkGallery.style.visibility = 'visible';
+                    break;
+                case studiesLink:
+                case studiesSpan:
+                    studiesGallery.style.visibility = 'visible';
+            }
+        })
+    }) 
+    //gallery navigation
     pImages.forEach(img => {
         img.addEventListener('click', (e) => {
             currentPaintCurrentImage.src = e.target.src;
             pImages.forEach(img => (img.style.opacity = 1));
             e.target.style.opacity = 0.6;
+            paintingInfo.innerHTML = e.target.nextElementSibling.innerHTML;
         });
     })
     dImages.forEach(img => {
@@ -106,10 +132,5 @@ window.onload = function(){
             e.target.style.opacity = 0.6;
         });
     })
-    //functions
-    
-
-
-
 
 };
